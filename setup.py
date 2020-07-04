0d72d961544634cd21dd0622acd6e8d5cdb9f2b3
MeerBiene <b.buhles@gmail.com>
2020-07-04 14:08:15 +0200

    updated setup script

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 # automatic vhost setup
 domain = input("Enter your domain (the one you used in the certbot command after the -d flag): ")
+ip = input("Enter the IP, where you want to proxy your traffic to now (for localhost use localhost or 127.0.0.1): ")
+port = input("Enter the desired port now: ")
 
 # input
 fin = open("example.com.conf", "rt")
@@ -9,8 +11,8 @@ domainfinal = domain + ".conf"
 fout = open(domainfinal, "wt")
 
 for line in fin:
-    fout.write(line.replace('example.com', domain))
-    
+    fout.write(line.replace('example.com', domain).replace('PROXIEDIP', ip).replace('PORT', port))
+
 fin.close()
 fout.close()
 print("Done.")
